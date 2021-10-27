e51c8213a1b8e847ac63976519224d553d7ebae9
elsalvacion <elsalvacion2022@gmail.com>
2021-10-28 00:52:36 +0800

    v1 ready for testing

--- routes/email.js
+++ routes/email.js
@@ -1,31 +1,40 @@
 const router = require("express").Router();
 const verifier = require("email-verify");
 
+router.get("/", (req, res) => res.send("Working"));
+
 // get filtered data
 router.post("/filter", (req, res) => {
   const emails = req.body.emails;
   const verifiedEmails = [];
   const wrongEmail = [];
+  let idx = 0;
+  console.log(emails.length);
   try {
-    emails.forEach((email) => {
+    emails.forEach((email, i) => {
       verifier.verify(email, function (err, info) {
         if (!info.success) wrongEmail.push(email);
         else {
           verifiedEmails.push(email);
         }
+
+        if (i + 1 === emails.length) {
+          res.json({
+            success: true,
+            wrongEmails: wrongEmail,
+            msg: verifiedEmails,
+            filtered: Math.ceil(
+              ((emails.length - verifiedEmails.length) / emails.length) * 100
+            ),
+          });
+        }
       });
+      idx++;
     });
 
-    setTimeout(() => {
-      res.json({
-        success: true,
-        wrongEmails: wrongEmail,
-        msg: verifiedEmails,
-        filtered: Math.ceil(
-          ((emails.length - verifiedEmails.length) / emails.length) * 100
-        ),
-      });
-    }, 15000);
+    // setTimeout(() => {
+
+    // }, 15000);
   } catch (err) {
     return res.status(500).json({ msg: "Server Error" });
   }
--- server.js
+++ server.js
@@ -23,13 +23,13 @@ app.use(fileupload());
 // express body parser
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.send(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "public", "index.html"));
+//   });
+// }
 app.use("/email", email);
 
 const PORT = process.env.PORT || 5000;
