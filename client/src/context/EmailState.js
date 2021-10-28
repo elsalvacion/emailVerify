8d81c9feef89b813dbe55ac4e3fc905e80a529ac
elsalvacion <elsalvacion2022@gmail.com>
2021-10-28 14:27:21 +0800

    v1 ready for testing

--- client/src/context/EmailState.js
+++ client/src/context/EmailState.js
@@ -24,15 +24,18 @@ const EmailState = (props) => {
       }
     });
 
-    const res = await fetch(`http://localhost:5000/email/filter`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        emails,
-      }),
-    });
+    const res = await fetch(
+      `https://correct-email.herokuapp.com/email/filter`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          emails,
+        }),
+      }
+    );
     const json = await res.json();
     console.log(json.wrongEmails.length, json.msg.length);
     if (json) {
